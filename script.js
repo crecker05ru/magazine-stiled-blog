2e6d43ad57e4845c1da8639ab60a5dae44aabb82
Anvar <fort2652@rambler.ru>
2020-12-31 17:54:12 +0300

    prikol add

---
+++ script.js
@@ -0,0 +1,9 @@
+let searchButton = document.querySelector(".header-search__button_icon");
+
+searchButton.addEventListener('click', function() {
+    alert("Чел,это макет сайта,что ты пытаешься здесь найти?")
+});
+
+//{
+//    alert("Чел,это макет сайта,что ты пытаешься здесь найти?")
+//}
